25e3c4cabae30e9171c6feda946a9281fa60acc4
Aleksey <alekseydevweb@gmail.com>
2025-02-25 22:00:55 +0300

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'node_modules'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   css: {
     preprocessorOptions: {
       scss: {
