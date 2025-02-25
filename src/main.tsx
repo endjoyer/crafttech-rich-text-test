77b471f4bc0084e3602c552d12d15bac232af57c
Aleksey <alekseydevweb@gmail.com>
2025-02-25 18:34:03 +0300

    fix: path in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from '@/store';
-import '@/styles/global.scss';
+import { store } from './store';
+import './styles/global.scss';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
